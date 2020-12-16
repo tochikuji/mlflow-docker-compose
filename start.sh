bd14c32cbf346939201196dd9420fda8527579fb
Aiga SUZUKI <tochikuji@gmail.com>
2020-12-16 15:50:23 +0900

    GCPを使わない設定に変更

--- start.sh
+++ start.sh
@@ -4,10 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-mlflow db upgrade $DB_URI
-
+mlflow db upgrade $DB_URI || \
 mlflow server \
     --backend-store-uri $DB_URI \
     --host 0.0.0.0 \
     --port 80 \
-    --default-artifact-root gs://$GCP_STORAGE_BUCKET
+    --default-artifact-root $ARTIFACT_LOCATION
